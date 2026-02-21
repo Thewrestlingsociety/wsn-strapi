c17ba86b6b7c66992908fa9277fad7b1a9ad1904
thewrestlingsociety <bradywrestlingsociety@gmail.com>
2026-02-21 17:34:14 -0600

    fix: add postrges config for render deployment

---
+++ config/env/production/database.ts
@@ -0,0 +1,13 @@
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      connectionString: env('DATABASE_URL'),
+      ssl: { rejectUnauthorized: false },
+    },
+    pool: {
+      min: 0,
+      max: 10,
+    },
+  },
+});
