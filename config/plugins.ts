180327eb850186a562d5782e4406aca6b0440071
Thewrestlingsociety <bradywrestlingsociety@gmail.com>
2026-03-03 19:27:18 -0600

    Enable Cloudflare R2 uploads

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "@strapi-community/strapi-provider-upload-cloudflare-r2",
+      providerOptions: {
+        accessKeyId: env("R2_ACCESS_KEY_ID"),
+        secretAccessKey: env("R2_SECRET_ACCESS_KEY"),
+        endpoint: env("R2_ENDPOINT"),
+        params: {
+          Bucket: env("R2_BUCKET"),
+        },
+      },
+    },
+  },
+});
