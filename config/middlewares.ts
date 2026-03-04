cee6412c3e0ab25302be94fcca540c323fff0990
Thewrestlingsociety <bradywrestlingsociety@gmail.com>
2026-03-04 00:08:48 -0600

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,16 +1,12 @@
-import type { Core } from '@strapi/strapi';
-
-const config: Core.Config.Middlewares = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+export default [
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
-
-export default config;
